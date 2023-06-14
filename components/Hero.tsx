5a47824435dd5d94114f4d8658c5be96a911f7e2
ThusharaSamaraweera <thusharadasun204@gmail.com>
2023-06-14 20:52:38 +0530

    add navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { CustomButton } from "./custom/CustomButton";
+import Image from "next/image";
 
 export default function Hero() {
   const handleScroll = () => {
@@ -11,12 +12,19 @@ export default function Hero() {
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">Find, book, rent a car—quick and super easy!</h1>
         <p className="hero__subtitle">Streamline your car rental experience with our effortless booking process.</p>
+
         <CustomButton
           title="Explore Cars"
-          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10 px-3 py-3"
           handleClick={handleScroll}
-          textStyles="px-3 py-5"
         />
+        <div className="hero__image-container">
+          <div className="hero__image">
+            <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          </div>
+
+          <div className="hero__image-overlay" />
+        </div>
       </div>
     </div>
   );
--- components/index.tsx
+++ components/index.tsx
@@ -1,7 +1,9 @@
 import Hero from "./Hero";
 import { CustomButton } from "./custom/CustomButton";
+import Navbar from "./Navbar";
 
 export {
     Hero,
-    CustomButton
+    CustomButton,
+    Navbar
 }
