1670f885a220301345bb060153f20b3640729838
ThusharaSamaraweera <thusharadasun204@gmail.com>
2023-06-14 20:28:23 +0530

    add custom btn

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,22 @@
+"use client";
 import React from "react";
 import { CustomButton } from "./custom/CustomButton";
 
 export default function Hero() {
+  const handleScroll = () => {
+  }
+
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">Find, book, rent a car—quick and super easy!</h1>
         <p className="hero__subtitle">Streamline your car rental experience with our effortless booking process.</p>
-        <CustomButton />
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+          textStyles="px-3 py-5"
+        />
       </div>
     </div>
   );
--- components/custom/CustomButton.tsx
+++ components/custom/CustomButton.tsx
@@ -1,7 +1,18 @@
+"use client";
+import { CustomButtonProps } from '@/types';
+import Image from 'next/image'
 import React from 'react'
 
-export const CustomButton = () => {
+export const CustomButton = (props: CustomButtonProps) => {
+  const { title, containerStyles, handleClick, isDisabled, btnType, rightIcon, textStyles } = props;
   return (
-    <div>CustomButton</div>
-  )
-}
+    <button
+      disabled={isDisabled}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+    </button>
+  );
+};
